7fe783e52eda16582540c2d1bd61099b67ebbace
alanjofy1997 <alan.jofy@Lindner-Group.com>
2026-01-27 08:50:59 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const API = "http://127.0.0.1:8000";
+const API = "https://lca-databank-backend.onrender.com";
 const container = document.getElementById("materialsContainer");
 const totalTableBody = document.querySelector("#totalTable tbody");
 const addBtn = document.getElementById("addMaterialBtn");
